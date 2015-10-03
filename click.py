43b6fbb0efd1b41ae13bb8563549ec00397e7935
Miss Siu <ksiu@thelangton.org.uk>
2015-10-03 07:27:28 +0100

    Continous movement, edge bouncing, scoring

--- click.py
+++ click.py
@@ -1,40 +1,76 @@
 from tkinter import *
-from random import randint
+from random import *
 import time, threading
+class Game:
+    def __init__(self):
+        root = Tk()
 
-root = Tk()
+        self.canvas = Canvas(root, width=500, height=500)
+        self.canvas.pack()
+        self.score = 0
+        self.ovals = {}
 
-canvas = Canvas(root, width=500, height=500)
-canvas.pack()
-
-def click(event):
-    if canvas.find_withtag(CURRENT):
-        canvas.itemconfig(CURRENT, fill="blue")
-        canvas.update_idletasks()
-        canvas.after(200)
-        canvas.delete(CURRENT)
-def move(event):
-    t = threading.Thread(target=start)
-    t.start()
-def start():
-    while True:
-        time.sleep(0.05)
-        for i in canvas.find_withtag("oval"):
-            
+        for i in range(100):
             num = randint(0,100)
             mod = 1 if num%2==0 else -1
-            pos = canvas.coords(i)
-            try:
-                canvas.coords(i, pos[0]+mod, pos[1]+mod, pos[2]+mod, pos[3]+mod)
-            except:
-                continue
+            num = randint(0,100)
+            mod2 = 1 if num%2==0 else -1
 
-for i in range(100):
-    x, y = randint(0, 500-1), randint(0, 500-1)
-    oval = canvas.create_oval(x-5, y-5, x+5, y+5, fill="red")
-    canvas.itemconfig(oval, tags=("oval",))
+            x, y = randint(0, 500-1), randint(0, 500-1)
+            oval = self.canvas.create_oval(x-10, y-10, x+10, y+10, fill="red")
+            self.ovals[oval] = (random()*mod,random()*mod2);
+            self.canvas.itemconfig(oval, tags=("oval",))
 
-canvas.bind("<Button-1>", click)
-canvas.bind("<Button-3>", move)
+        self.canvas.bind("<Button-1>", self.click)
+        self.canvas.bind("<Button-3>", self.move)
+        root.mainloop()
 
-root.mainloop()
+    def click(self, event):
+        if self.canvas.find_withtag(CURRENT):
+            self.canvas.itemconfig(CURRENT, fill="blue")
+            self.canvas.update_idletasks()
+            self.canvas.after(20)
+            pos = self.canvas.coords(CURRENT)
+            self.score = self.score+int((10/abs(pos[0]-pos[2])*100))
+            self.canvas.delete(CURRENT)
+            print (self.score)
+        else:
+            self.score = self.score - 100
+            print(self.score)
+    def move(self, event):
+        t = threading.Thread(target=self.start)
+        t.start()
+    def start(self):
+        while True:
+            self.canvas.update_idletasks()
+            self.canvas.after(5)
+            for i in self.canvas.find_withtag("oval"):
+                
+                num = randint(0,100)
+                mod = 1 if num%2==0 else -1
+                num = randint(0,100)
+                mod2 = 1 if num%2==0 else -1
+                
+                pos = self.canvas.coords(i)
+                try:
+                    self.canvas.coords(i, pos[0], pos[1], pos[2]+mod2, pos[3]+mod2)
+                except:
+                    continue
+                x = self.ovals.get(i)[0]
+                y = self.ovals.get(i)[1]
+                try:
+                    pos = self.canvas.coords(i)
+                    if pos[0] > 490 or pos[0] < 10:
+                        x = x*-1
+                    if pos[1] > 490 or pos[1] < 10:
+                        y = y*-1
+                    self.ovals[i] = (x, y)
+               
+                
+                    self.canvas.coords(i, pos[0]+x, pos[1]+y, pos[2]+x, pos[3]+y)
+                except:
+                    continue
+            
+Game()            
+                
+    
